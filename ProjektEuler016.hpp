4699ffa1e689f6dd61d7c078a9bad18d196e2eef
Fytch <josua.rieder1996@gmail.com>
2016-08-18 17:41:53 +0200

    ProjektEuler016.hpp: general improvements
    - gave it a more sensible name (integer instead of bit)
    - replaced spaces with tabs
    - correctly implemented /, /=, ++ (pre and post), -- (pre and post), <,
    <=, >, >=, ==, !=
    - made operators that don't need to be members free
    - implemented some testing in _main()
    - made the class a class template which allows the user to choose the
    bit width

--- ProjektEuler016.hpp
+++ ProjektEuler016.hpp
@@ -6,245 +6,303 @@
 #include <iostream>
 #include <bitset>
 #include <algorithm>
+#include <stdexcept>
 namespace PE016
 {
-    class myBit
-    {
-        std::bitset<8> Data;
+	template<std::size_t N>
+	class BasicInteger
+	{
+		std::bitset<N> Data;
 
-    public:
-        myBit()
-        {
+	public:
+		BasicInteger()
+			: BasicInteger(0)
+		{
+		}
+		BasicInteger(unsigned long long x)
+			: Data(x)
+		{
+		}
 
-        }
-        myBit(unsigned long val): Data(val)
-        {
-        }
-        unsigned long to_ulong() const
-        {
-            return this->Data.to_ulong();
-        }
+		unsigned long to_ulong() const
+		{
+			return this->Data.to_ulong();
+		}
 
-        void invert()
-        {
-            this->Data.flip();
-        }
+		void invert()
+		{
+			this->Data.flip();
+		}
 
-        myBit& operator/=(myBit const& Rhs)
-        {
-            unsigned counter = this->Data.size()-1;
-            while(this->Data[counter] != 1)
-            {
-                --counter;
-            }
-            std::cout << "\ncounter: " << counter << "\n\n";
+		BasicInteger& operator+= (BasicInteger const& Rhs)
+		{
+			std::bitset<1> carry(0);
+			for(unsigned i = 0; i < this->Data.size(); ++i)
+			{
+				std::bitset<1> atemp(this->Data[i]);
+				this->Data[i] = this->Data[i] ^ Rhs.Data[i] ^ carry[0];
+				carry[0] = (atemp[0] & Rhs.Data[i]) | (atemp[0] & carry[0]) | (carry[0] & Rhs.Data[i]);
+			}
+			return *this;
+		}
+		BasicInteger& operator-= (BasicInteger const& Rhs)
+		{
+			BasicInteger tempRhs = Rhs;
+			tempRhs.invert();
+			++tempRhs;
+			return *this += tempRhs;
+		}
+		BasicInteger& operator*= (BasicInteger const& Rhs)
+		{
+			BasicInteger Result(0);
+			for(unsigned i = 0; i < this->Data.size(); ++i)
+			{
+				BasicInteger temp(0);
+				for(unsigned j = 0; j < Rhs.Data.size(); ++j)
+				{
+					temp.Data[j] = this->Data[i] & Rhs.Data[j];
+				}
+				temp.Data <<= i;
+				Result += temp;
+			}
+			*this = Result;
+			return *this;
+		}
+		BasicInteger& operator/= (BasicInteger Rhs)
+		{
+			if(Rhs == 0)
+			{
+				throw std::domain_error("BasicInteger::operator/=: division by zero");
+			}
 
-            myBit Result(0);
-            std::cout << "Result:   " << Result << '\n';
-            myBit Quotient(1);
-            //--counter;
-            std::cout << "Quotient: " << Quotient << '\n';
-            std::cout << "-------------------------------\n";
-            while(counter > 1)
-            {
-                std::cout << "this:     " << *this << '\n';
-                std::cout << "Rhs:      " << Rhs << '\n';
-                std::cout << "Quotient: " << Quotient << '\n';
-                std::cout << "Result:   " << Result << '\n';
-                std::cout << "-------------------------------\n";
-                while(Rhs > Quotient)
-                {
-                    if(counter > 1)
-                        --counter;
-                    else
-                        break;
-                    Quotient.Data<<=1;
-                    Quotient.Data[0] = this->Data[counter];
-                    Result.Data<<=1;
-                    std::cout << "Rhs:      " << Rhs << '\n';
-                    std::cout << "Quotient: " << Quotient << '\n';
-                    std::cout << "Result:   " << Result << '\n';
-                    std::cout << "-------------------------------\n";
-                }
+			if(Rhs > *this)
+			{
+				return *this = 0;
+			}
+			else
+			{
+				BasicInteger Result = 0;
 
-             //   Quotient-=Rhs;
-              //  Result.Data[0] = true;
-               // --counter;*/
-            }
+				std::size_t MSB;
+				for(MSB = this->Data.size() - 1; MSB != 0 && !this->Data[MSB]; --MSB);
 
-            return  *this = Result;
-        }
+				int RhsShifts;
+				for(RhsShifts = 0; !Rhs.Data[MSB]; ++RhsShifts, Rhs.Data <<= 1);
+				
+				for(; *this != BasicInteger(0) && RhsShifts >= 0; --RhsShifts, Rhs.Data >>= 1)
+				{
+					if(Rhs <= *this)
+					{
+						*this -= Rhs;
+						Result.Data[RhsShifts] = true;
+					}
+				}
+				return *this = Result;
+			}
+		}
 
-        myBit& operator*=(myBit const& Rhs)
-        {
-            myBit Result(0);
-            for(unsigned i = 0; i < this->Data.size(); ++i)
-            {
-                myBit temp(0);
-                for(unsigned j = 0; j < Rhs.Data.size(); ++j)
-                {
-                    temp.Data[j] = this->Data[i] & Rhs.Data[j];
-                }
-                temp.Data<<=i;
-                Result +=temp;
-            }
-            *this = Result;
-            return *this;
-        }
+		friend std::ostream& operator<< (std::ostream& Stream, BasicInteger const& Object)
+		{
+			Stream << Object.Data;
+			return Stream;
+		}
 
-        myBit& operator-=(myBit const& Rhs)
-        {
-           myBit tempRhs = Rhs;
-           tempRhs.invert();
-           ++tempRhs;
-           return *this += tempRhs;
-        }
-        myBit& operator++()
-        {
-            return *this += myBit(1);
-        }
-        myBit& operator+=(myBit const& Rhs)
-        {
-            std::bitset<1> carry(0);
-            for(unsigned i = 0; i < this->Data.size(); ++i)
-            {
-                std::bitset<1> atemp(this->Data[i]);
-                this->Data[i] = this->Data[i] ^ Rhs.Data[i] ^ carry[0];
-                carry[0] = (atemp[0] & Rhs.Data[i]) | (atemp[0] & carry[0]) | (carry[0] & Rhs.Data[i]);
-            }
-            return *this;
-        }
+		bool operator== (BasicInteger const& Rhs) const
+		{
+			return this->Data == Rhs.Data;
+		}
+		bool operator< (BasicInteger const& Rhs) const
+		{
+			for(std::size_t i = this->Data.size(); i-- != 0;)
+			{
+				if(this->Data[i] && !Rhs.Data[i])
+				{
+					return false;
+				}
+				else if(!this->Data[i] && Rhs.Data[i])
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	};
 
-        myBit& operator=(myBit const& Rhs)
-        {
-            this->Data = Rhs.Data;
-            return *this;
-        }
+	template<std::size_t N>
+	BasicInteger<N> operator+ (BasicInteger<N> Lhs, BasicInteger<N> const& Rhs)
+	{
+		return Lhs += Rhs;
+	}
+	template<std::size_t N>
+	BasicInteger<N> operator- (BasicInteger<N> Lhs, BasicInteger<N> const& Rhs)
+	{
+		return Lhs -= Rhs;
+	}
+	template<std::size_t N>
+	BasicInteger<N> operator* (BasicInteger<N> Lhs, BasicInteger<N> const& Rhs)
+	{
+		return Lhs *= Rhs;
+	}
+	template<std::size_t N>
+	BasicInteger<N> operator/ (BasicInteger<N> Lhs, BasicInteger<N> const& Rhs)
+	{
+		return Lhs /= Rhs;
+	}
 
-        friend myBit operator/(myBit Lhs, myBit const& Rhs)
-        {
-            return Lhs/=Rhs;
-        }
+	template<std::size_t N>
+	BasicInteger<N>& operator++ (BasicInteger<N>& Object)
+	{
+		return Object += 1;
+	}
+	template<std::size_t N>
+	BasicInteger<N> operator++ (BasicInteger<N>& Object, int)
+	{
+		const BasicInteger<N> Result = Object;
+		++Object;
+		return Result;
+	}
 
-        friend myBit operator*(myBit Lhs, myBit const& Rhs)
-        {
-            return Lhs*=Rhs;
-        }
-        friend myBit operator+(myBit Lhs, myBit const& Rhs)
-        {
-            return Lhs += Rhs;
-        }
+	template<std::size_t N>
+	BasicInteger<N>& operator-- (BasicInteger<N>& Object)
+	{
+		return Object -= 1;
+	}
+	template<std::size_t N>
+	BasicInteger<N> operator-- (BasicInteger<N>& Object, int)
+	{
+		const BasicInteger<N> Result = Object;
+		--Object;
+		return Result;
+	}
 
-        friend myBit operator-(myBit Lhs, myBit const& Rhs)
-        {
-            return Lhs -= Rhs;
-        }
+	template<std::size_t N>
+	bool operator!= (BasicInteger<N> const& Lhs, BasicInteger<N> const& Rhs)
+	{
+		return !(Lhs == Rhs);
+	}
+	template<std::size_t N>
+	bool operator> (BasicInteger<N> const& Lhs, BasicInteger<N> const& Rhs)
+	{
+		return Rhs < Lhs;
+	}
+	template<std::size_t N>
+	bool operator<= (BasicInteger<N> const& Lhs, BasicInteger<N> const& Rhs)
+	{
+		return !(Lhs > Rhs);
+	}
+	template<std::size_t N>
+	bool operator>= (BasicInteger<N> const& Lhs, BasicInteger<N> const& Rhs)
+	{
+		return !(Lhs < Rhs);
+	}
 
-        friend std::ostream& operator<< (std::ostream& Stream, myBit const& Object)
-        {
-            Stream << Object.Data;
-            return Stream;
-        }
+	typedef BasicInteger<8> Int8;
 
-        friend bool operator> (myBit const& Lhs, myBit const& Rhs)
-        {
-        for (unsigned i = Rhs.Data.size(); i > 0; --i)
-        {
-            if (Lhs.Data[i] > Rhs.Data[i])
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        }
-        friend bool operator< (myBit const& Lhs, myBit const& Rhs)
-        {
-            return !(Rhs > Lhs);
-        }
+	void _main()
+	{
+		auto Div = [&](Int8 const& Lhs, Int8 const& Rhs)
+		{
+			std::cout << Lhs.to_ulong() << '/' << Rhs.to_ulong() << " = " << (Lhs / Rhs).to_ulong() << '\n';
+		};
+		Div(0, 1);
+		Div(8, 2);
+		Div(2, 8);
+		Div(29, 5);
+		Div(29, 29);
 
-    };
+		auto Less = [&](Int8 const& Lhs, Int8 const& Rhs)
+		{
+			std::cout << Lhs.to_ulong() << '<' << Rhs.to_ulong() << " ? " << (Lhs < Rhs ? "yea" : "nah") << '\n';
+		};
+		Less(0, 0);
+		Less(0, 1);
+		Less(3, 4);
+		Less(4, 4);
+		Less(5, 4);
 
+		/*
+		const myBit a(15);
+		const myBit b(16);
+		myBit c(240);
+		*/
 
-    void _main()
-    {
-        const myBit a(15);
-        const myBit b(16);
-        myBit c(240);
-      /*  std::cout <<"s = a+b\n";
-        std::cout << "a: " << a.to_ulong() << "\t Bin: " << a << '\n';
-        std::cout << "b: " << b.to_ulong() << "\t Bin: " << b << '\n';
-        c = a+b;
-        std::cout << "S: " << c.to_ulong() << "\t Bin: " << c << "\n\n";
+		/*
+		std::cout <<"s = a+b\n";
+		std::cout << "a: " << a.to_ulong() << "\t Bin: " << a << '\n';
+		std::cout << "b: " << b.to_ulong() << "\t Bin: " << b << '\n';
+		c = a+b;
+		std::cout << "S: " << c.to_ulong() << "\t Bin: " << c << "\n\n";
 
-        std::cout <<"s = c-a\n";
-        std::cout << "c: " << c.to_ulong() << "\t Bin: " << c << '\n';
-        std::cout << "a: " << a.to_ulong() << "\t Bin: " << a << '\n';
-        c = c-a;
-        std::cout << "S: " << c.to_ulong() << "\t Bin: " << c << "\n\n";
+		std::cout <<"s = c-a\n";
+		std::cout << "c: " << c.to_ulong() << "\t Bin: " << c << '\n';
+		std::cout << "a: " << a.to_ulong() << "\t Bin: " << a << '\n';
+		c = c-a;
+		std::cout << "S: " << c.to_ulong() << "\t Bin: " << c << "\n\n";
 
-        std::cout <<"s = a*b\n";
-        std::cout << "a: " << a.to_ulong() << "\t Bin: " << a << '\n';
-        std::cout << "b: " << b.to_ulong() << "\t Bin: " << b << '\n';
-        c = a*b;
-        std::cout << "S: " << c.to_ulong() << "\t Bin: " << c << "\n\n";
+		std::cout <<"s = a*b\n";
+		std::cout << "a: " << a.to_ulong() << "\t Bin: " << a << '\n';
+		std::cout << "b: " << b.to_ulong() << "\t Bin: " << b << '\n';
+		c = a*b;
+		std::cout << "S: " << c.to_ulong() << "\t Bin: " << c << "\n\n";
 
-        std::cout << "a < b\n";
-        std::cout <<  a.to_ulong() << " < " <<  b.to_ulong() << " : ";
-        bool asdf = a<b;
-        std::cout << asdf << "\n";
-        std::cout << "a > b\n";
-        std::cout <<  a.to_ulong() << " > " <<  b.to_ulong() << " : ";
-        asdf = a>b;
-        std::cout << asdf << "\n";*/
+		std::cout << "a < b\n";
+		std::cout <<  a.to_ulong() << " < " <<  b.to_ulong() << " : ";
+		bool asdf = a<b;
+		std::cout << asdf << "\n";
+		std::cout << "a > b\n";
+		std::cout <<  a.to_ulong() << " > " <<  b.to_ulong() << " : ";
+		asdf = a>b;
+		std::cout << asdf << "\n";
+		*/
 
-        std::cout <<"s=s/b\n";
-        std::cout << "s: " << c.to_ulong() << "\t Bin: " << c << '\n';
-        std::cout << "b: " << b.to_ulong() << "\t Bin: " << b << '\n';
-        c/=b;
-        std::cout << "S: " << c.to_ulong() << "\t Bin: " << c << "\n\n";
+		/*
+		std::cout <<"s=s/b\n";
+		std::cout << "s: " << c.to_ulong() << "\t Bin: " << c << '\n';
+		std::cout << "b: " << b.to_ulong() << "\t Bin: " << b << '\n';
+		c/=b;
+		std::cout << "S: " << c.to_ulong() << "\t Bin: " << c << "\n\n";
+		*/
 
+		/*
+		unsigned sum = 0;
+		unsigned counter = 0;
+		myBit _x(2);
+		myBit RTemp(1);
 
-   /*     unsigned sum = 0;
-        unsigned counter = 0;
-        myBit _x(2);
-        myBit RTemp(1);
+		for(unsigned i = 0; i <1000; ++i)
+		{
+			_x*=myBit(2);
+		}
 
-        for(unsigned i = 0; i <1000; ++i)
-        {
-            _x*=myBit(2);
-        }
+		while(_x > RTemp)
+		{
+			RTemp *= myBit(10);
+			++counter;
+		}
+		--counter;
+		RTemp = myBit(0);
 
-        while(_x > RTemp)
-        {
-            RTemp *= myBit(10);
-            ++counter;
-        }
-        --counter;
-        RTemp = myBit(0);
+		for(unsigned i = 0; i <counter; ++i)
+		{
+			RTemp *= myBit(10);
+		}
 
-        for(unsigned i = 0; i <counter; ++i)
-        {
-            RTemp *= myBit(10);
-        }
+		while(_x > 0)
+		{
+			while(_x > RTemp)
+			{
+				_x -= RTemp;
+				++sum;
+			}
+			--counter;
+			RTemp = myBit(0);
 
-        while(_x > 0)
-        {
-            while(_x > RTemp)
-            {
-                _x -= RTemp;
-                ++sum;
-            }
-            --counter;
-            RTemp = myBit(0);
-
-            for(unsigned i = 0; i <counter; ++i)
-            {
-                RTemp *= myBit(10);
-            }
-        }
-        std::cout << sum ;*/
-    }
+			for(unsigned i = 0; i <counter; ++i)
+			{
+				RTemp *= myBit(10);
+			}
+		}
+		std::cout << sum ;
+		*/
+	}
 }
