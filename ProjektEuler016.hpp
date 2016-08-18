2939c1d3a8213e24c61d40939460ce4cbd7f674e
SwissXD <tobiaskeller1995@gmail.com>
2016-08-18 13:36:31 +0200

    operator /

--- ProjektEuler016.hpp
+++ ProjektEuler016.hpp
@@ -25,9 +25,54 @@ namespace PE016
             return this->Data.to_ulong();
         }
 
-        void flip()
+        void invert()
         {
             this->Data.flip();
+        }
+
+        myBit& operator/=(myBit const& Rhs)
+        {
+            unsigned counter = this->Data.size()-1;
+            while(this->Data[counter] != 1)
+            {
+                --counter;
+            }
+            std::cout << "\ncounter: " << counter << "\n\n";
+
+            myBit Result(0);
+            std::cout << "Result:   " << Result << '\n';
+            myBit Quotient(1);
+            //--counter;
+            std::cout << "Quotient: " << Quotient << '\n';
+            std::cout << "-------------------------------\n";
+            while(counter > 1)
+            {
+                std::cout << "this:     " << *this << '\n';
+                std::cout << "Rhs:      " << Rhs << '\n';
+                std::cout << "Quotient: " << Quotient << '\n';
+                std::cout << "Result:   " << Result << '\n';
+                std::cout << "-------------------------------\n";
+                while(Rhs > Quotient)
+                {
+                    if(counter > 1)
+                        --counter;
+                    else
+                        break;
+                    Quotient.Data<<=1;
+                    Quotient.Data[0] = this->Data[counter];
+                    Result.Data<<=1;
+                    std::cout << "Rhs:      " << Rhs << '\n';
+                    std::cout << "Quotient: " << Quotient << '\n';
+                    std::cout << "Result:   " << Result << '\n';
+                    std::cout << "-------------------------------\n";
+                }
+
+             //   Quotient-=Rhs;
+              //  Result.Data[0] = true;
+               // --counter;*/
+            }
+
+            return  *this = Result;
         }
 
         myBit& operator*=(myBit const& Rhs)
@@ -50,15 +95,13 @@ namespace PE016
         myBit& operator-=(myBit const& Rhs)
         {
            myBit tempRhs = Rhs;
-           tempRhs.flip();
+           tempRhs.invert();
            ++tempRhs;
            return *this += tempRhs;
         }
         myBit& operator++()
         {
-             myBit val(1);
-            *this += val;
-            return *this;
+            return *this += myBit(1);
         }
         myBit& operator+=(myBit const& Rhs)
         {
@@ -77,6 +120,12 @@ namespace PE016
             this->Data = Rhs.Data;
             return *this;
         }
+
+        friend myBit operator/(myBit Lhs, myBit const& Rhs)
+        {
+            return Lhs/=Rhs;
+        }
+
         friend myBit operator*(myBit Lhs, myBit const& Rhs)
         {
             return Lhs*=Rhs;
@@ -113,8 +162,9 @@ namespace PE016
         }
         friend bool operator< (myBit const& Lhs, myBit const& Rhs)
         {
-            return Rhs < Lhs;
+            return !(Rhs > Lhs);
         }
+
     };
 
 
@@ -122,8 +172,8 @@ namespace PE016
     {
         const myBit a(15);
         const myBit b(16);
-        myBit c(0);
-        std::cout <<"s = a+b\n";
+        myBit c(240);
+      /*  std::cout <<"s = a+b\n";
         std::cout << "a: " << a.to_ulong() << "\t Bin: " << a << '\n';
         std::cout << "b: " << b.to_ulong() << "\t Bin: " << b << '\n';
         c = a+b;
@@ -140,5 +190,61 @@ namespace PE016
         std::cout << "b: " << b.to_ulong() << "\t Bin: " << b << '\n';
         c = a*b;
         std::cout << "S: " << c.to_ulong() << "\t Bin: " << c << "\n\n";
+
+        std::cout << "a < b\n";
+        std::cout <<  a.to_ulong() << " < " <<  b.to_ulong() << " : ";
+        bool asdf = a<b;
+        std::cout << asdf << "\n";
+        std::cout << "a > b\n";
+        std::cout <<  a.to_ulong() << " > " <<  b.to_ulong() << " : ";
+        asdf = a>b;
+        std::cout << asdf << "\n";*/
+
+        std::cout <<"s=s/b\n";
+        std::cout << "s: " << c.to_ulong() << "\t Bin: " << c << '\n';
+        std::cout << "b: " << b.to_ulong() << "\t Bin: " << b << '\n';
+        c/=b;
+        std::cout << "S: " << c.to_ulong() << "\t Bin: " << c << "\n\n";
+
+
+   /*     unsigned sum = 0;
+        unsigned counter = 0;
+        myBit _x(2);
+        myBit RTemp(1);
+
+        for(unsigned i = 0; i <1000; ++i)
+        {
+            _x*=myBit(2);
+        }
+
+        while(_x > RTemp)
+        {
+            RTemp *= myBit(10);
+            ++counter;
+        }
+        --counter;
+        RTemp = myBit(0);
+
+        for(unsigned i = 0; i <counter; ++i)
+        {
+            RTemp *= myBit(10);
+        }
+
+        while(_x > 0)
+        {
+            while(_x > RTemp)
+            {
+                _x -= RTemp;
+                ++sum;
+            }
+            --counter;
+            RTemp = myBit(0);
+
+            for(unsigned i = 0; i <counter; ++i)
+            {
+                RTemp *= myBit(10);
+            }
+        }
+        std::cout << sum ;*/
     }
 }
